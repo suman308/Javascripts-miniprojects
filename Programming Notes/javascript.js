68241fdb0545c72171e85b3e302817ba05b7ef89
Suman <neeilkamal308@gmail.com>
2021-04-29 21:04:17 -0700

    static characters of the javascript class is added

--- Programming Notes/javascript.js
+++ Programming Notes/javascript.js
@@ -247,3 +247,28 @@ console.log(num) ==> 1
                 const pentagon = new Polygon(1,2,3,4,5);
 
                 console.log([...pentagon.getSides()]); // [1,2,3,4,5]
+              d) The class constant or the class variable and also the class function is written in the javascript by adding the static in front of the then 
+                class Point {
+                      constructor(x, y) {
+                        this.x = x;
+                        this.y = y;
+                      }
+
+                      static displayName = "Point";
+                      static distance(a, b) {
+                        const dx = a.x - b.x;
+                        const dy = a.y - b.y;
+
+                        return Math.hypot(dx, dy);
+                      }
+                    }
+
+                    const p1 = new Point(5, 5);
+                    const p2 = new Point(10, 10);
+                    p1.displayName; // undefined
+                    p1.distance;    // undefined
+                    p2.displayName; // undefined
+                    p2.distance;    // undefined
+
+                    console.log(Point.displayName);      // "Point"
+                    console.log(Point.distance(p1, p2)); // 7.0710678118654755
