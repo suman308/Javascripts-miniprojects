3c0e96bad5849cac9604adf0054b4d53629b8ad3
Suman <neeilkamal308@gmail.com>
2021-04-29 18:16:10 -0700

    the new class example added

--- Programming Notes/javascript.js
+++ Programming Notes/javascript.js
@@ -232,4 +232,18 @@ console.log(num) ==> 1
                   const p = new Rectangle(); // ReferenceError
 
                       class Rectangle {}
-                  
+              c) class Polygon {
+                  constructor(...sides) {
+                    this.sides = sides;
+                  }
+                  // Method
+                  *getSides() {
+                    for(const side of this.sides){
+                      yield side;
+                    }
+                  }
+                }
+
+                const pentagon = new Polygon(1,2,3,4,5);
+
+                console.log([...pentagon.getSides()]); // [1,2,3,4,5]
