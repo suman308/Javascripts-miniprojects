66714dd0adcf9caa152bd4686b8b84a43ba0cc83
Suman <neeilkamal308@gmail.com>
2021-12-29 15:27:14 -0500

    the new html message element is added to the page using the node.js

--- eShop/app.js
+++ eShop/app.js
@@ -1,5 +1,11 @@
 const http  = require('http'); 
 const server = http.createServer((req, res)=> {
-    console.log(req)
-})
-server.listen()
+    
+    res.setHeader('Content-type', 'text/html'); 
+    res.write('<html>'); 
+    res.write('<head><title>"My first Page"</title></head>');
+    res.write('<body><h1>"this is my first page made in the node.js"</h1></body>' ); 
+    res.end();
+     
+});
+server.listen(3000);
