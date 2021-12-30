45d34228f4489b164637507406582f3b8bb01f8e
Suman <neeilkamal308@gmail.com>
2021-12-29 20:11:12 -0500

    the router is created

--- eShop/app.js
+++ eShop/app.js
@@ -1,11 +1,14 @@
 const http  = require('http'); 
 const server = http.createServer((req, res)=> {
-    
+    const url = req.url 
+    const method = req.method;
+    if(url === '/'){
     res.setHeader('Content-type', 'text/html'); 
     res.write('<html>'); 
     res.write('<head><title>"My first Page"</title></head>');
-    res.write('<body><h1>"this is my first page made in the node.js"</h1></body>' ); 
-    res.end();
-     
+    res.write('<body><h1>"this is my first page made in the node.js"</h1> <form action="/message" type="text"> <button type="submit"></button></form> </body>' ); 
+    return res.end();
+    } 
+    if(url ==='/')
 });
 server.listen(3000);
