0cea17b2207729f48667e734e841be6e9bb4d44f
Suman <neeilkamal308@gmail.com>
2022-01-04 19:54:03 -0500

    the comment is removed

--- eShop/app.js
+++ eShop/app.js
@@ -2,4 +2,4 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.header);
 });
-server.listen(3000,()=> console.log(req.url)) // this 
+server.listen(3000,()=> console.log(req.url)) 
