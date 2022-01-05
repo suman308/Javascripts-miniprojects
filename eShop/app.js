e2a054d2e1caaf26806d53d4700a29b33fffada9
Suman <neeilkamal308@gmail.com>
2022-01-04 19:53:34 -0500

    the new comment is added

--- eShop/app.js
+++ eShop/app.js
@@ -1,19 +1,5 @@
-const http  = require('http'); 
-const server = http.createServer((req, res)=> {
-    const url = req.url 
-    
-    console.log(url)
-    const method = req.method;
-    if(url === '/'){
-    res.setHeader('Content-type', 'text/html'); 
-    res.write('<html>'); 
-    res.write('<head><title>"My first Page"</title></head>');
-    res.write('<body><h1>"this is my first page made in the node.js"</h1> <form action="/message"  method="POST" type="text"><input type="text"></input> <button type="submit"> send</button></form> </body>' ); 
-    res.end();
-    } 
-    if(url ==='/message' && method=="POST"){
-        const body = []; 
-        
-    }
+const http = require('http'); 
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.header);
 });
-server.listen(3000);
+server.listen(3000,()=> console.log(req.url)) // this 
