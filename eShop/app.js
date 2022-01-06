3c5b96923bb8bce6f87d27537c9d8ade394e1805
Suman <neeilkamal308@gmail.com>
2022-01-06 16:29:39 -0500

    the jason file is added

--- eShop/app.js
+++ eShop/app.js
@@ -1,9 +1,9 @@
 const http = require('http'); 
 const server = http.createServer((req, res)=> {
-const url = req.url;
-console.log(url);
+
+console.log(req.url, req.method, req.headers);
 }); 
 
 server.listen(3000, ()=> {
-    console.log( "server started at the port 3000")
+    console.log( "server started at the port 4000")
 })
