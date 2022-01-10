2607e394eaad2da47ddd6747f0ae5c4d42a4a139
Suman <neeilkamal308@gmail.com>
2022-01-10 15:32:07 -0500

    the express is added

--- eShop/app.js
+++ eShop/app.js
@@ -1,10 +1,3 @@
 const http = require('http'); 
-const server = http.createServer((req, res)=> {
-const url = req.url
-console.log(req.url, req.method, req.headers);
-}); 
-
-server.listen(3000, ()=> {
-     console.log( "server started at the port 4000") // server.listen will direct the webpage to listen to the port 3000
-})
-
+const express = require('express')
+const app = express(); 
