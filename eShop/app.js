e48ed159b2286cc367bb625da578f1780d1add96
Suman <neeilkamal308@gmail.com>
2022-01-10 15:34:12 -0500

    the server is created on express'

--- eShop/app.js
+++ eShop/app.js
@@ -1,3 +1,11 @@
 const http = require('http'); 
 const express = require('express')
 const app = express(); 
+
+app.use( (req, res, next) => {
+    req.send('<h> hello there </h>')
+    console.log
+})
+
+
+servr.listen(3000)
