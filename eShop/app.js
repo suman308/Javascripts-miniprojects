93605333951c73955e3ae92aed279b8e7ebe3a97
Suman <neeilkamal308@gmail.com>
2022-02-14 15:55:52 -0500

    the routing of the req is done to send the res

--- eShop/app.js
+++ eShop/app.js
@@ -1,9 +1,18 @@
 const http = require('http'); 
 const server = http.createServer((req, res)=> {
 console.log(req.url, req.method);
-    res.write('<html><p>suman a</p></html>')
-    res.setHeader('Content-Type','text/html');
-    res.write('<p>hello there </p>');
+
+ const url = req.url; 
+ if (url == '/') {
+     res.write('<html> <p1> This is the first page </p1>  </html> ')
+ } else if( url =='/new'){
+     res.write('<html> <p1> This is the second page page </p1>  </html> ')
+ } else if( url =='/notnew'){
+     res.write('<html> <p1> This is the third page </p1>  </html> ')
+ }
+   else {
+     res.write('<html> <p1> 404 error </p1>  </html> ')
+   }
 });
 
 server.listen(3000);
