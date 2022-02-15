520e3bbed81f0996e3d5e1f208ab42608d8eef50
Suman <neeilkamal308@gmail.com>
2022-02-15 14:11:23 -0500

    the new form is added

--- eShop/app.js
+++ eShop/app.js
@@ -3,13 +3,19 @@ const server = http.createServer((req, res)=> {
 console.log(req.url, req.method);
 
  const url = req.url; 
+ const method = req.method 
  if (url == '/') {
      res.write('<html> <p1> This is the first page </p1>  </html> ')
  } else if( url =='/new'){
      res.write('<html> <p1> This is the second page page </p1>  </html> ')
  } else if( url =='/notnew'){
      res.write('<html> <p1> This is the third page </p1>  </html> ')
+     res.write('<html><h1>Thank you for the post </h1></html>')
+
+ } else if( url =='/notenew' && method == ' POST'){
+     res.write('<html><h1>Thank you for the post </h1></html>'); 
  }
+ 
    else {
      res.write('<html> <p1> 404 error </p1>  </html> ')
    }
